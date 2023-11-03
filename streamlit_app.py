db3b83e57e1db0a075444e98b1bf19e88cd6d7b9
tennisj93 <149627877+tennisj93@users.noreply.github.com>
2023-11-03 12:17:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ try:
             streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
-streamlit.error()
+      streamlit.error()
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
